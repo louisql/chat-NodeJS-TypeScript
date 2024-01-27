f73689b88c85f96ccf69d9053c28abebdc842aec
Louis <louis@planhub.ca>
2024-01-26 20:42:02 -0500

    Debugged first test

--- src/__tests__/auth.test.ts
+++ src/__tests__/auth.test.ts
@@ -1,16 +1,23 @@
 import { registerUser } from "../controllers/auth";
 
 test('registration should return success message', async() =>{
-    const req = {
+    const req: any = {
         body:{
             username: 'testuser',
             email: 'testemail@test.com',
             password: 'testpassword'
         }
-    }
+    } as unknown as Request
 
+    const res: any = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn()
+    } as unknown as Response;
     
-    const response = await registerUser();
-    expect(response.status).toBe(201);
-    expect(response.body.message).toBe('User registered successfully');
-}
+    // Call the registerUser function with the mocked objects
+    await registerUser(req, res);
+
+    // Perform assertions on the response object
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.json).toHaveBeenCalledWith({ message: 'User registered successfully' });
+});
