a8ff30c9779133145d07fb9f0ccbd1105ca03806
LouisTest <louistest@planhubs-MacBook-Air.local>
2024-02-01 20:22:41 -0500

    changing jest parameters and Atlas bd info

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ module.exports = {
     },
     moduleNameMapper: {
       '^@/(.*)$': '<rootDir>/src/$1',
+      "^(\\.\\.?\\/.+)\\.js$": "$1"
     },
     testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
